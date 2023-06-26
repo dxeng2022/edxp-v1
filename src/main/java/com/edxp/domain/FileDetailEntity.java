57b954743e71f760c7535250bd25b8ad383bd140
epike <epikeran@gmail.com>
2023-06-26 09:12:21 +0900

    feat: Add file upload code

---
+++ src/main/java/com/edxp/domain/FileDetailEntity.java
@@ -0,0 +1,55 @@
+package com.edxp.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.time.Instant;
+
+@Getter
+@Table(name = "\"FileDetail\"")
+@Entity
+public class FileDetailEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter @Column(nullable = false) private Long userId;
+    @Setter @Column(nullable = false) private String fileName;
+    @Setter @Column(nullable = false) private String fileOriginalName;
+    @Setter @Column(nullable = false) private Long fileSize;
+    @Setter @Column(nullable = false) private String filePath;
+    private Timestamp registeredAt;
+
+    protected FileDetailEntity() {}
+
+    public FileDetailEntity(
+            Long userId,
+            String fileName,
+            String fileOriginalName,
+            Long fileSize,
+            String filePath
+    ) {
+        this.userId = userId;
+        this.fileName = fileName;
+        this.fileOriginalName = fileOriginalName;
+        this.fileSize = fileSize;
+        this.filePath = filePath;
+    }
+
+    @PrePersist
+    void registeredAt() {
+        this.registeredAt = Timestamp.from(Instant.now());
+    }
+
+    public static FileDetailEntity of(
+            Long userId,
+            String fileName,
+            String fileOriginalName,
+            Long fileSize,
+            String filePath
+    ) {
+        return new FileDetailEntity(userId, fileName, fileOriginalName, fileSize, filePath);
+    }
+}
---
+++ src/main/java/com/edxp/service/FileService.java
@@ -0,0 +1,13 @@
+package com.edxp.service;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class FileService {
+    private final AmazonS3Client amazonS3Client;
+}
