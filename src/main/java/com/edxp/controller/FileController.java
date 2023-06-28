df4c18e43c6b0c8a1177c394c0da7a27c1020535
epike <epikeran@gmail.com>
2023-06-28 13:24:33 +0900

    feat: Add make folder interface

--- src/main/java/com/edxp/controller/FileController.java
+++ src/main/java/com/edxp/controller/FileController.java
@@ -5,6 +5,7 @@ import com.edxp.common.response.CommonResponse;
 import com.edxp.config.auth.PrincipalDetails;
 import com.edxp.constant.ErrorCode;
 import com.edxp.dto.request.FileDeleteRequest;
+import com.edxp.dto.request.FileFolderRequest;
 import com.edxp.dto.request.FileUploadRequest;
 import com.edxp.dto.response.FileListResponse;
 import com.edxp.exception.EdxpApplicationException;
@@ -30,22 +31,12 @@ public class FileController {
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
-//    @GetMapping
-//    public CommonResponse<List<?>> getFileList(
-//            @RequestParam(required = false) String currentPath,
-//            @AuthenticationPrincipal PrincipalDetails principal
-//    ) {
-//        if(principal == null) throw new EdxpApplicationException(ErrorCode.USER_NOT_LOGIN);
-//        List<?> files = fileService.getFiles(principal.getUser().getId(), currentPath);
-//        return CommonResponse.success(files);
-//    }
-
     @GetMapping
     public CommonResponse<List<FileListResponse>> getFileList(
             @RequestParam(required = false) String currentPath,
             @AuthenticationPrincipal PrincipalDetails principal
     ) {
-        if(principal == null) throw new EdxpApplicationException(ErrorCode.USER_NOT_LOGIN);
+        if (principal == null) throw new EdxpApplicationException(ErrorCode.USER_NOT_LOGIN);
         List<FileListResponse> files = fileService.getFiles(principal.getUser().getId(), currentPath);
         return CommonResponse.success(files);
     }
@@ -63,14 +54,25 @@ public class FileController {
     }
 
     @CrossOrigin
+    @PostMapping("/add-folder")
+    public CommonResponse<Void> addFolder(
+            @RequestBody FileFolderRequest request,
+            @AuthenticationPrincipal PrincipalDetails principal
+    ) {
+        if (principal == null) throw new EdxpApplicationException(ErrorCode.USER_NOT_LOGIN);
+        fileService.addFolder(principal.getUser().getId(), request);
+        return CommonResponse.success();
+    }
+
+    @CrossOrigin
     @PostMapping
     public CommonResponse<Void> uploadFile(
-            @RequestPart(value = "data")  FileUploadRequest request,
+            @RequestPart(value = "data") FileUploadRequest request,
             @RequestPart(value = "file") MultipartFile file,
             @AuthenticationPrincipal PrincipalDetails principal
     ) {
-        if(principal == null) throw new EdxpApplicationException(ErrorCode.USER_NOT_LOGIN);
-        if(file == null) throw new EdxpApplicationException(ErrorCode.FILE_NOT_ATTACHED);
+        if (principal == null) throw new EdxpApplicationException(ErrorCode.USER_NOT_LOGIN);
+        if (file == null) throw new EdxpApplicationException(ErrorCode.FILE_NOT_ATTACHED);
         fileService.uploadFile(principal.getUser().getId(), new FileUploadRequest(request.getCurrentPath(), file));
         return CommonResponse.success();
     }
@@ -78,7 +80,8 @@ public class FileController {
     @CrossOrigin
     @DeleteMapping
     public CommonResponse<Void> deleteFile(@RequestBody FileDeleteRequest request) {
-        fileService.deleteFile(request);
+        boolean isSuccess = fileService.deleteFile(request);
+        if (!isSuccess) throw new EdxpApplicationException(ErrorCode.FILE_NOT_FOUND);
         return CommonResponse.success();
     }
 }
