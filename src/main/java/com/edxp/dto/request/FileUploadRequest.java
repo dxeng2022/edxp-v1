4679d0892531ca9f6c63c663646206abfe0567c2
epike <epikeran@gmail.com>
2023-07-03 13:22:54 +0900

    feat: Modify file list like explorer and Add multi upload

--- src/main/java/com/edxp/dto/request/FileUploadRequest.java
+++ src/main/java/com/edxp/dto/request/FileUploadRequest.java
@@ -5,10 +5,12 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 @Getter
 @Setter
 @AllArgsConstructor
 public class FileUploadRequest {
     private String currentPath;
-    private MultipartFile file;
+    private List<MultipartFile> files;
 }
