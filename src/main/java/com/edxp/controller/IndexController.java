3ed49a5836db514fda77cb052c571a7001b1ee14
epike <epikeran@gmail.com>
2023-07-18 14:59:15 +0900

    feat: Change upload folder

--- src/main/java/com/edxp/controller/IndexController.java
+++ src/main/java/com/edxp/controller/IndexController.java
@@ -2,7 +2,6 @@ package com.edxp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
@@ -12,17 +11,12 @@ public class IndexController {
     }
 
     @GetMapping(value = {
-            "/module", "/mypage",
-            "/module/draw", "/module/sheet", "/module/doc",
+            "/module/**", "/mypage/**",
+            "/module/draw/**", "/module/sheet/**", "/module/doc/**",
             "/module/draw/download", "/module/sheet/download", "/module/doc/choice",
             "/module/draw/cloud", "/module/sheet/cloud", "/module/doc/cloud"
     })
     public String modulePage() {
-        return "index.html";
-    }
-
-    @GetMapping("/sign")
-    public String signPage() {
         return "index.html";
     }
 }
