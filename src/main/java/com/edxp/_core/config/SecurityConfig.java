de83919e16a4fcb1b3b782d1c012ae4e705ededd
epike <epikeran@gmail.com>
2023-10-05 09:17:13 +0900

    fix: Modify to multi login

--- src/main/java/com/edxp/_core/config/SecurityConfig.java
+++ src/main/java/com/edxp/_core/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.edxp._core.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,12 +14,11 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
+@Slf4j
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
@@ -31,20 +31,22 @@ public class SecurityConfig {
 //                        ).authenticated()
                                 .anyRequest().permitAll()
                 )
-                .sessionManagement(sessionManagement -> sessionManagement
-                            .maximumSessions(1)
-                            .maxSessionsPreventsLogin(true)
-                            .expiredUrl("/session-expired")
-                            .sessionRegistry(sessionRegistry())
-                )
+//                .sessionManagement(sessionManagement -> sessionManagement
+//                            .maximumSessions(1)
+//                            .maxSessionsPreventsLogin(true)
+//                            .expiredUrl("/session-expired")
+//                            .sessionRegistry(sessionRegistry())
+//                )
                 .formLogin()
                     .successHandler((request, response, authentication) -> {
+                        log.info("로그인 성공");
                         // 로그인 성공 시 JSON 응답을 리턴
                         response.setStatus(HttpServletResponse.SC_OK);
                         response.setContentType("application/json");
                         response.getWriter().write("{\"success\": true}");
                     })
                     .failureHandler((request, response, exception) -> {
+                        log.info("로그인 실패");
                         // 로그인 실패 시 JSON 응답을 리턴
                         response.setStatus(HttpServletResponse.SC_NOT_FOUND);
                         response.setContentType("application/json");
@@ -54,12 +56,21 @@ public class SecurityConfig {
                 .and()
                 .logout()
                     .logoutUrl("/logout") // 로그아웃 URL 설정
-                    .addLogoutHandler((request, response, authentication) -> {
-                        HttpSession session = request.getSession();
-                        if (session != null) {
-                            session.invalidate();
-                        }
-                    })
+                    .deleteCookies("JSESSIONID")
+                    .invalidateHttpSession(true)
+                    .clearAuthentication(true)
+//                    .addLogoutHandler((request, response, authentication) -> {
+//                        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//                        if (principal instanceof PrincipalDetails) {
+//                            UserDetails userDetails = (PrincipalDetails) principal;
+//                            List<SessionInformation> sessions = sessionRegistry().getAllSessions(userDetails, false);
+//
+//                            // 세션 정보를 만료시켜 SessionRegistry 를 최신화
+//                            for (SessionInformation session : sessions) {
+//                                session.expireNow();
+//                            }
+//                        }
+//                    })
                     .logoutSuccessHandler((request, response, authentication) -> {
                         // 로그아웃 성공 시 처리 (옵션)
                         response.setStatus(HttpServletResponse.SC_OK);
