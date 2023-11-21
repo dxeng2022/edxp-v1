1f7ab7ccfd67b1e99743aee1a6ae264abb41ca23
epike <epikeran@gmail.com>
2023-11-21 10:55:37 +0900

    chore: Edit test deploy process

--- src/main/java/com/edxp/_core/config/SecurityConfig.java
+++ src/main/java/com/edxp/_core/config/SecurityConfig.java
@@ -55,7 +55,8 @@ public class SecurityConfig {
 //                    .anyRequest().permitAll()
                 .and()
                 .formLogin()
-//                    .loginPage("/")
+                    .loginPage("/")
+                    .loginProcessingUrl("/login")
                     .successHandler((request, response, authentication) -> {
                         log.info("로그인 성공");
                         // 로그인 성공 시 JSON 응답을 리턴
