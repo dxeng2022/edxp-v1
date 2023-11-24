ceb425f52ad27541dbb90506226929881e4315d6
epike <epikeran@gmail.com>
2023-11-24 15:43:29 +0900

    fix: login error fix

--- src/main/java/com/edxp/controller/IndexController.java
+++ src/main/java/com/edxp/controller/IndexController.java
@@ -1,15 +1,15 @@
 package com.edxp.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @RequiredArgsConstructor
 @Controller
-public class IndexController {
-    @GetMapping({"/"})
+public class IndexController implements ErrorController {
+    @GetMapping({"/", "/error"})
     public String index() {
         return "index.html";
     }
@@ -17,10 +17,5 @@ public class IndexController {
     @GetMapping(value = {"/module/**", "/mypage/**"})
     public String modulePage() {
         return "index.html";
-    }
-
-    @RequestMapping(value = "/{path:[^\\.]*}")
-    public String redirect() {
-        return "forward:/";
     }
 }
