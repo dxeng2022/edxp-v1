80614a2cdc57b943f0ef2a5fe6301472593a76e9
jake <epikeran@gmail.com>
2024-04-08 09:54:28 +0900

    feat: Proxy image url logic

---
+++ src/main/java/com/edxp/_core/controller/ImageProxyController.java
@@ -0,0 +1,54 @@
+package com.edxp._core.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+@RequestMapping("/proxy-image")
+@Controller
+public class ImageProxyController {
+    @Value("${aws.s3.url}")
+    private String AWS_S3_URL;
+
+    private final Map<String, String> imageUrls = new HashMap<>();
+
+    @GetMapping
+    public void proxyImage(
+            @RequestParam(name = "imageKey") String imageKey,
+            HttpServletResponse response
+    ) {
+        // 이미지 URL 캐싱된 경우, 캐시된 URL 사용
+        String imageUrl = imageUrls.computeIfAbsent(imageKey, AWS_S3_URL::concat);
+
+        try (InputStream inputStream = new URL(imageUrl).openStream()) {
+            response.setContentType("image/png");
+            StreamUtils.copy(inputStream, response.getOutputStream());
+        } catch (IOException e) {
+            handleImageFetchError(response);
+        }
+    }
+
+    private void handleImageFetchError(HttpServletResponse response) {
+        response.setStatus(HttpStatus.NOT_FOUND.value());
+        response.setContentType("image/png");
+
+        try (InputStream errorImageStream = getClass().getResourceAsStream("/img/errorImage.png")) {
+            if (errorImageStream != null) {
+                StreamUtils.copy(errorImageStream, response.getOutputStream());
+            } else {
+                response.getOutputStream().write(new byte[0]);
+            }
+        } catch (IOException ignored) { }
+    }
+}
