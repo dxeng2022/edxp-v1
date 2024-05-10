f19496571153f500ed879b23ad1e630b138c5e2d
jake <epikeran@gmail.com>
2024-05-10 12:48:17 +0900

    refactor: Add header content response

--- src/main/java/com/edxp/order/doc/controller/OrderDocController.java
+++ src/main/java/com/edxp/order/doc/controller/OrderDocController.java
@@ -17,12 +17,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
@@ -180,10 +182,13 @@ public class OrderDocController {
     @CrossOrigin
     @PostMapping("/visual-loc")
     public CommonResponse<OrderDocRiskResponse> visualLocal(
-            @RequestParam("file") MultipartFile file
+            @RequestParam("file") MultipartFile file,
+            HttpServletResponse httpServletResponse
     ) throws IOException {
         if (file == null) throw new EdxpApplicationException(ErrorCode.FILE_NOT_ATTACHED);
         OrderDocRiskResponse response = orderDocBusiness.visualizationLocal(file);
+
+        httpServletResponse.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + file.getOriginalFilename());
 
         return CommonResponse.success(response);
     }
