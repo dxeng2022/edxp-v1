aed5116fe66cfb37c940b298798202dbc29536c9
jake <epikeran@gmail.com>
2024-06-24 12:31:26 +0900

    fix: Fix sign up api to open

--- src/main/java/com/edxp/_core/constant/AddressType.java
+++ src/main/java/com/edxp/_core/constant/AddressType.java
@@ -4,7 +4,7 @@ public class AddressType {
     public static String[] OPEN_ADDRESS = {
             "/", "/log", "/login",
             "/isLogin", "/public-key", "/log",
-            "/api/v1/user/signup", "/api/v1/user/check-dupl", "/api/v1/user/signup-auth", "/api/v1/user/signup-authcheck",
+            "/api/v1/user/sign-up", "/api/v1/user/check-dupl", "/api/v1/user/signup-auth", "/api/v1/user/signup-authcheck",
             "/api/v1/user/find-mail",  "/api/v1/user/find-pw"
     };
 }
